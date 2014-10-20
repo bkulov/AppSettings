62a07733d93f1dbfb8760d81fedd1e3086188ddf
Borislav Kulov <Borislav.Kulov@telerik.local>
2014-10-20 12:05:11 +0300

    Read JSON from file.

--- AppSettings_Test/js/index.js
+++ AppSettings_Test/js/index.js
@@ -27,6 +27,15 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        
+        var loadButton = document.getElementById("loadBtn");
+        loadButton.addEventListener("click", function() {
+          loadJSON(function(response) {
+            // Parse JSON string into object
+            var actual_JSON = JSON.parse(response);
+            console.log(actual_JSON);
+          });
+        });
     },
     // deviceready Event Handler
     //
@@ -48,3 +57,16 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+function loadJSON(callback) {   
+  var xobj = new XMLHttpRequest();
+      xobj.overrideMimeType("application/json");
+	xobj.open('GET', 'MySettings.json', true);
+	xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
